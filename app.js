1f6ee6e647e2dd5f9a395494d43d78eaf67d535f
Ruben Orrostieta <ruben.orrostieta@gmail.com>
2022-04-23 15:51:02 -0500

    add: POST method and ran with postman

--- app.js
+++ app.js
@@ -24,6 +24,12 @@ app.get('/v1/explorers/:id', (req,res) =>{
   res.status(200).json(explorer)
 })
 
+app.post('/v1/explorers', (req,res) =>{
+  console.log(`API Explorers POST request ${new Date()}`)
+  const requestBody = req.body // Parametros de un cliente
+  res.status(201).json({message:'Created'})
+})
+
 // Con esto inicializamos esta app
 app.listen(port, () => {
  console.log(`Example app listening on port ${port}`)
